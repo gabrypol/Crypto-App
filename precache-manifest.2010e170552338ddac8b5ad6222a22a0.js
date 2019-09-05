bf91c75f16057e2f7218365b659f10fc5d86f31d
gabrypol <gabrielepolenta@gmail.com>
2019-09-05 07:56:31 +0200

    Updates

---
+++ precache-manifest.2010e170552338ddac8b5ad6222a22a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96237f88dde49802ca1c41a366470c7c",
+    "url": "/Crypto-App/index.html"
+  },
+  {
+    "revision": "a8193ee74d0bf9c47f1b",
+    "url": "/Crypto-App/static/css/main.13e740a5.chunk.css"
+  },
+  {
+    "revision": "05efcd67d4aa8e382e98",
+    "url": "/Crypto-App/static/js/2.ff1092d0.chunk.js"
+  },
+  {
+    "revision": "a8193ee74d0bf9c47f1b",
+    "url": "/Crypto-App/static/js/main.09f5f215.chunk.js"
+  },
+  {
+    "revision": "887f38893097d2841e82",
+    "url": "/Crypto-App/static/js/runtime~main.0b07252b.js"
+  }
+]);
